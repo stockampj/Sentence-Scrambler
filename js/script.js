0eb8c4704986a6da96195373951ed141b41b9fff
Student <example@example.com>
2019-08-13 18:37:06 -0700

    add more functions to cipher

--- js/script.js
+++ js/script.js
@@ -1,18 +1,55 @@
 jQuery(document).ready (function () {
 
-    var userSentence= prompt("Type a sentence.");
-    // var userSentence = "lorem ipsum dolor sit amet" ;
+  // let userSentence= prompt("Type a sentence.");
+  let userSentence = "lorem ipsum dolor sit amet?";
+  let workingSentence = userSentence.replace(/[.,\/#!$%?\^&\*;:{}=\-_`~()]/g,"");
 
-    var sentenceLength = userSentence.length;
-    var minusOuterCharacters = userSentence.slice(1,sentenceLength-1)
+  function scramble1 (text) {
+    let sentenceLength = text.length;
+    let sansOuterCharacters= text.slice(1,sentenceLength-1);
+    let firstLetter = text.charAt(0);
+    let firstLetterUpper = firstLetter.toUpperCase();
+    let lastLetter = text.charAt(sentenceLength-1)
+    let lastLetterUpper = lastLetter.toUpperCase();
+    return firstLetterUpper + sansOuterCharacters + lastLetterUpper;
+  }
 
-    var firstLetter = userSentence.charAt(0);
-    var firstLetterUpper = firstLetter.toUpperCase();
-    var lastLetter = userSentence.charAt(sentenceLength-1);
-    var lastLetterUpper = lastLetter.toUpperCase();
-    var transform = firstLetterUpper + minusOuterCharacters + lastLetterUpper;
+  function scramble2 (text) {
+    let sentenceLength = text.length;
+    let firstLetter = text.charAt(0);
+    let firstLetterUpper = firstLetter.toUpperCase();
+    let lastLetter = text.charAt(sentenceLength-1)
+    let lastLetterUpper = lastLetter.toUpperCase();
+    return lastLetterUpper + firstLetterUpper;
+  }
 
-alert(transform);
+  function scramble3 (text) {
+    return scramble1(text) + scramble2(text);
+  }
+
+  function scramble4 (text) {
+    let midCutStart = text.length/2;
+    let midCut = Math.floor(midCutStart);
+    let slideText = text.charAt(midCut);
+    return slideText + scramble3(text);
+  }
+
+  function scramble5 (text) {
+
+  }
+
+  var result = scramble4(workingSentence);
+
+    // var sentenceLength = userSentence.length;
+    // var minusOuterCharacters = userSentence.slice(1,sentenceLength-1)
+    //
+    // var firstLetter = userSentence.charAt(0);
+    // var firstLetterUpper = firstLetter.toUpperCase();
+    // var lastLetter = userSentence.charAt(sentenceLength-1);
+    // var lastLetterUpper = lastLetter.toUpperCase();
+    // var transform = firstLetterUpper + minusOuterCharacters + lastLetterUpper;
+
+  alert(result);
 
 
 
